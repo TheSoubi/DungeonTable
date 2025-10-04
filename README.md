ce55b5d9f70671a58b0ac8044081b9d3dd5ad4a8
TheSoubiy <85482416+TheSoubi@users.noreply.github.com>
2025-10-04 21:08:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Dungeon Table
+
+Dungeon Table is a super efficient virtual table top software optimized for in-person and local role playing games around a table. It is meant to be intuitive, fast and beautiful.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Game />
+  </div>
+</template>
+
+<script>
+import Game from './components/Game.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Game
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
+  background: #f5f5f5;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
